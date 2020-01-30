83ff3699df7b37b05bd4460f3ec309f205a854b2
Michael Pedruski <mpedruski@gmail.com>
2020-01-30 10:39:43 -0500

    Refined class balance, added relaxed accuracy metric reporting

--- scripts/13-Decision_tree_classification.py
+++ scripts/13-Decision_tree_classification.py
@@ -17,6 +17,7 @@ import logging
 import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
+np.random.seed(31415)
 
 ### Regression based on historical data
 
@@ -31,17 +32,21 @@ df = pd.read_csv(file_to_open)
 
 
 ### Find length of majority and minority class data
-minority_class_len = len(df[df['Demand']==1])
-majority_class_indices = df[df['Demand']==0].index
+minority_class_len = len(df[df['Demand']==2])
+majority_class_1_indices = df[df['Demand']==0].index
+majority_class__2_indices = df[df['Demand']==1].index
 
 ### Generate a list of majority class indices to retain, based on how many minority
 ### class data their are
-random_majority_indices = np.random.choice(majority_class_indices, minority_class_len,
+random_majority_1_indices = np.random.choice(majority_class_1_indices, minority_class_len,
     replace = False)
+random_majority_2_indices = np.random.choice(majority_class__2_indices, minority_class_len,
+        replace = False)
 ### Make list of minority class indices and combine with majority class indices
 ### then define dataset as the rows of the dataset from those indices
-minority_class_indices = df[df['Demand']>0].index
-under_sample_indices = np.concatenate([minority_class_indices,random_majority_indices])
+minority_class_indices = df[df['Demand']>1].index
+under_sample_indices = np.concatenate([minority_class_indices,random_majority_1_indices,
+    random_majority_2_indices])
 df = df.loc[under_sample_indices]
 
 
@@ -56,27 +61,28 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_
 
 ## Classify features on variable to predict, and test against
 ### test set
-regr_1 = DecisionTreeClassifier(max_depth=7, min_samples_leaf=10)
+regr_1 = DecisionTreeClassifier(max_depth=16, min_samples_leaf=8)
 regr_1_model = regr_1.fit(x_train, y_train)
 y_pred = regr_1.predict(x_test)
+
+### Validate the model
+### Straight up precision just says how many items were exactly right
 print(regr_1.score(x_test,y_test))
-print(confusion_matrix(y_test, y_pred))
-print(sum(confusion_matrix(y_test, y_pred)))
-# scores = np.zeros((7,19))
-# for i in range(3,10):
-#     for j in range(1,20):
-#         regr_1 = DecisionTreeClassifier(max_depth=j, min_samples_leaf=i)
-#         regr_1_model = regr_1.fit(x_train, y_train)
-#         # y_1 = regr_1.predict(x_test)
-#         scores[i-3,j-1]=regr_1.score(x_test,y_test)
-#         # print(R2)
-#     # print(y_1.min(),y_1.max())
-# print(np.amax(scores))
-# parameters = np.unravel_index(scores.argmax(), scores.shape)
-# print(parameters)
-# plt.figure()
-# regr_1 = DecisionTreeClassifier(max_depth=parameters[1]+1, min_samples_leaf=parameters[0]+3)
-# regr_1.fit(x_train, y_train)
-# print(regr_1.score(x_test,y_test))
-# plot_tree(regr_1, filled=True)
-# plt.savefig('Regression_tree_output.pdf',format='pdf')
+### Confusion matrix shows many items are misclassified as closely related values
+### (i.e. 4 as 3 or 5, which is an acceptable margin of error for my problem).
+### Accounting for this wider margin, the accuracy is close to 0.7
+conf_mat = confusion_matrix(y_test, y_pred)
+print(conf_mat)
+s = []
+print(conf_mat.shape)
+for i in range(len(conf_mat)):
+    if i == 0:
+        s.append(conf_mat[i,i]+conf_mat[i,i+1])
+    elif i == conf_mat.shape[0]-1:
+        s.append(conf_mat[i,i]+conf_mat[i, i-1])
+    else:
+        s.append(conf_mat[i,i]+conf_mat[i,i-1]+conf_mat[i,i+1])
+print(s)
+total_sum = sum(sum(conf_mat))
+print(sum(s)/total_sum)
+print(np.unique(y_test))
